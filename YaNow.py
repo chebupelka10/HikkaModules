d51b5c0b4f1aff677cad875db252653b22172e2d
Egor <141219716+chebupelka10@users.noreply.github.com>
2024-02-17 00:20:31 +0300

    Фикс багов + кастом оформление

--- YaNow.py
+++ YaNow.py
@@ -1,26 +1,28 @@
+# meta developer: @chepuxmodules
+
 from .. import loader, utils
 
 @loader.tds
 class YaNowMod(loader.Module):
-    """Interact with YaNowBot"""
+    """Модуль который показывает что вы слушаете сейчас на https://music.yandex.ru/ by @y9chepux"""
     strings = {"name": "YaNow"}
 
     async def ynowcmd(self, message):
-        """Send inline query to YaNowBot and click the first result"""
-        await utils.answer(message, "Sending inline query to YaNowBot...")
+        """Показывает что вы слушаете на https://music.yandex.ru/"""
+        await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Собираю данные о том что вы слушаете на https://music.yandex.ru/</b>")
         results = await message.client.inline_query("@YaNowBot", "")
         if results:
             await results[0].click(message.to_id, hide_via=True)
             await message.delete()
         else:
-            await utils.answer(message, "No results from YaNowBot")
+            await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, попробуйте позже повторить запрос</b>")
 
     async def ysettokencmd(self, message):
-        """Send /token command to YaNowBot with arguments"""
+        """Комманда чтобы сохранить ваш токен для входа в https://music.yandex.ru/. Инструкция как его получить: https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781/. Пример: .ysettoken VASH_TOKEN"""
         args = utils.get_args(message)
         if args:
-            await utils.answer(message, "Sending /token command to YaNowBot...")
+            await utils.answer(message, "<emoji document_id=5307973935927663936>✅</emoji><b>Токен был успешно сохранен!</b>")
             await message.client.send_message("@YaNowBot", "/token " + " ".join(args))
             await message.delete()
         else:
-            await utils.answer(message, "Please provide some arguments for /token command")
+            await utils.answer(message, "<emoji document_id=5312526098750252863>❌</emoji><b>Вы забыли указать token(Инструкция как его получить: https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781)")
