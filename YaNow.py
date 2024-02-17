9019acc5f823c73bfe28f0905d3eb941c8071323
Egor <141219716+chebupelka10@users.noreply.github.com>
2024-02-17 09:33:01 +0300

    Global update, add yanowtrack

--- YaNow.py
+++ YaNow.py
@@ -17,6 +17,16 @@ class YaNowMod(loader.Module):
         else:
             await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, попробуйте позже повторить запрос</b>")
 
+    async def yanowtrackcmd(self, message):
+        """Отправляет трек который вы слушаете на https://music.yandex.ru/"""
+        await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Собираю данные о том что вы слушаете на https://music.yandex.ru/</b>")
+        results = await message.client.inline_query("@YaNowBot", "")
+        if results:
+            await results[1].click(message.to_id, hide_via=True)
+            await message.delete()
+        else:
+            await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, попробуйте позже повторить запрос</b>")    
+    
     async def yasettokencmd(self, message):
         """Комманда чтобы сохранить ваш токен для входа в https://music.yandex.ru/. Инструкция как его получить: https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781/. Пример: .yasettoken VASH_TOKEN"""
         args = utils.get_args(message)
