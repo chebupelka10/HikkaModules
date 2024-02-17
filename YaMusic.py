1bc48a2400485d38a96781b7bee71a95675840f3
Egor <141219716+chebupelka10@users.noreply.github.com>
2024-02-17 16:06:49 +0300

    Наконец-то баг был полностью вырезан из модуля

--- YaMusic.py
+++ YaMusic.py
@@ -1,55 +1,58 @@
-# meta developer: @chepuxmodules
-
 from .. import loader, utils
 
 @loader.tds
 class YaMusicMod(loader.Module):
     """Модуль который показывает что вы слушаете сейчас на https://music.yandex.ru/ by @y9chepux"""
-    strings = {"name": "YaNow"}
+    strings = {"name": "YaMusic"}
 
     async def yanowcmd(self, message):
         """Показывает что вы слушаете на https://music.yandex.ru/"""
-        await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Собираю данные о том что вы слушаете на https://music.yandex.ru/</b>")
-        results = await message.client.inline_query("@YaNowBot", "")
+        await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Собираю данные о том, что вы слушаете на https://music.yandex.ru/</b>")
         try:
-            if results:
-                await results[0].click(message.to_id, hide_via=True)
-                await message.delete()
+            results = await message.client.inline_query("@YaNowBot", "")
+            await results[0].click(message.to_id, hide_via=True)
+            await message.delete()
+        except Exception as e:
+            if "The bot did not answer to the callback query in time" in str(e):
+                await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, вы слушаете трек в моей волне или вы не указали токен (Посмотрите help yamusic)</b>")
             else:
-                await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, вы слушаете трек в моей волне или вы не указали токен (Посмотри help yamusic)</b>")
-        except:
-            await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, вы слушаете трек в моей волне или вы не указали токен (Посмотри help yamusic)</b>")
-            return
+                await utils.answer(message, f"<emoji document_id=5314591660192046611>❌</emoji><b>Произошла ошибка: {e}</b>")
 
     async def yanowtrackcmd(self, message):
-        """Отправляет трек который вы слушаете на https://music.yandex.ru/. Чтобы это работало надо боту @YaNowBot отправить /settings и указать в поле стандартный ответ (нет)"""
-        await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Собираю данные о том что вы слушаете на https://music.yandex.ru/</b>")
-        results = await message.client.inline_query("@YaNowBot", "")
-        if results:
+        """Отправляет трек, который вы слушаете на https://music.yandex.ru/. Чтобы это работало, боту @YaNowBot нужно отправить /settings и указать в поле стандартный ответ (нет)"""
+        await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Собираю данные о том, что вы слушаете на https://music.yandex.ru/</b>")
+        try:
+            results = await message.client.inline_query("@YaNowBot", "")
             await results[1].click(message.to_id, hide_via=True)
             await message.delete()
-        else:
-            await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, вы слушаете трек в моей волне или вы не указали токен (Посмотри help yamusic)</b>")    
-    
+        except Exception as e:
+            if "The bot did not answer to the callback query in time" in str(e):
+                await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, вы слушаете трек в моей волне или вы не указали токен (Посмотрите help yamusic)</b>")
+            else:
+                await utils.answer(message, f"<emoji document_id=5314591660192046611>❌</emoji><b>Произошла ошибка: {e}</b>")
+
     async def yasearchcmd(self, message):
         """Ищет треки на https://music.yandex.ru/"""
         args = utils.get_args(message)
         if args:
             await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Ищу трек на https://music.yandex.ru/</b>")
-            results = await message.client.inline_query("@YaNowBot", " ".join(args))
-            if results:
+            try:
+                results = await message.client.inline_query("@YaNowBot", " ".join(args))
                 await results[0].click(message.to_id, hide_via=True)
                 await message.delete()
-            else:
-                await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, попробуйте позже повторить запрос или вы не указали токен (Посмотри help yanow)</b>")
+            except Exception as e:
+                if "The bot did not answer to the callback query in time" in str(e):
+                    await utils.answer(message, "<emoji document_id=5314591660192046611>❌</emoji><b>Ошибка, попробуйте позже повторить запрос или вы не указали токен (Посмотрите help yanow)</b>")
+                else:
+                    await utils.answer(message, f"<emoji document_id=5314591660192046611>❌</emoji><b>Произошла ошибка: {e}</b>")
         else:
             await utils.answer(message, "<emoji document_id=5312526098750252863>❌</emoji><b>Вы не указали название песни</b>")
-    
+
     async def yasettokencmd(self, message):
-        """Комманда чтобы сохранить ваш токен для входа в https://music.yandex.ru/. Инструкция как его получить: https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781/. Пример: .yasettoken VASH_TOKEN"""
+        """Команда чтобы сохранить ваш токен для входа в https://music.yandex.ru/. Инструкция как его получить: https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781/. Пример: .yasettoken VASH_TOKEN"""
         args = utils.get_args(message)
         if args:
-            await utils.answer(message, "<emoji document_id=5307973935927663936>✅</emoji><b>Токен был успешно сохранен! Если после этого комманды не работают проверьте чат @YaNowBot</b>")
+            await utils.answer(message, "<emoji document_id=5307973935927663936>✅</emoji><b>Токен был успешно сохранен! Если после этого команды не работают, проверьте чат @YaNowBot</b>")
             await message.client.send_message("@YaNowBot", "/token " + " ".join(args))
         else:
             await utils.answer(message, "<emoji document_id=5312526098750252863>❌</emoji><b>Вы забыли указать токен (Инструкция как его получить: https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781)</b>")
