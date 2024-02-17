1d33c5f8c94362048ca1b87acf0d950216a68e35
Egor <141219716+chebupelka10@users.noreply.github.com>
2024-02-17 12:52:54 +0300

    Если проблема в запятой то я самый умный

--- YaNow.py
+++ YaNow.py
@@ -32,7 +32,7 @@ class YaMusicMod(loader.Module):
         args = utils.get_args(message)
         if args:
             await utils.answer(message, "<emoji document_id=5463424079568584767>🎧</emoji><b>Ищу трек на https://music.yandex.ru/</b>")
-            results = await message.client.inline_query("@YaNowBot" + " ".join(args))
+            results = await message.client.inline_query("@YaNowBot", " ".join(args))
             if results:
                 await results[0].click(message.to_id, hide_via=True)
                 await message.delete()
