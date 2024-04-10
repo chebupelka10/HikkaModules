dea4c100535e6b3196f0da3fae93377467dbe8f1
Egor <141219716+chebupelka10@users.noreply.github.com>
2024-04-10 19:40:18 +0300

    Update ChepuxGPT.py

--- ChepuxGPT.py
+++ ChepuxGPT.py
@@ -36,7 +36,7 @@ class ChepuxGPTMod(loader.Module):
 
         prompt = [{"role": "user", "content": question}]
 
-        await message.edit("<b><emoji document_id=5323333310208810193>😀</emoji> Генерирую ответ...</b>")
+        await message.edit("<b><emoji document_id=5409143295039252230>🔄</emoji> Генерирую ответ...</b>")
         try:
             client = openai.AsyncOpenAI(api_key=api_key)
             response = await client.chat.completions.create(
@@ -44,6 +44,6 @@ class ChepuxGPTMod(loader.Module):
                 messages=prompt
             )
             answer = response.choices[0].message.content
-            await utils.answer(message, f"<b><emoji document_id=5314378951936711868>👍</emoji> Вопрос:</b> {question}\n<b><emoji document_id=5321366563079595431>👍</emoji> Ответ:</b> {answer}")
+            await utils.answer(message, f"<b><emoji document_id=5409229529392618973>🤔</emoji> Вопрос:</b> {question}\n<b><emoji document_id=5327958075158568158>💃</emoji> Ответ:</b> {answer}")
         except Exception as e:
             await utils.answer(message, f"<b><emoji document_id=5314591660192046611>❌</emoji> Произошла ошибка:</b> {e}")
