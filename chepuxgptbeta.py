68e048f698ee95326d8e3f3cb1aff222dd3698e1
chepuxcat <141219716+chebupelka10@users.noreply.github.com>
2024-06-11 06:30:34 -0700

    Update chepuxgptbeta.py

--- chepuxgptbeta.py
+++ chepuxgptbeta.py
@@ -5,12 +5,14 @@ from .. import loader, utils
 @loader.tds
 class ChepuxGPTBetaMod(loader.Module):
     """Позволяет разговаривать с ChatGPT без api key"""
+    is_generating = False  # Флаг состояния для проверки, идет ли генерация ответа
 
     strings = {
         "name": "ChepuxGPT",
         "generating": "Генерирую ответ...",
         "question": "Вопрос: {}",
         "answer": "Ответ: {}",
+        "busy": "Подождите, пока будет сгенерирован предыдущий ответ."
     }
 
     async def client_ready(self, client, db):
@@ -19,46 +21,49 @@ class ChepuxGPTBetaMod(loader.Module):
     @loader.command()
     async def gpt(self, message: types.Message):
         """Новая тема + задать вопрос"""
+        if self.is_generating:
+            await utils.answer(message, self.strings("busy"))
+            return
+
         query = utils.get_args_raw(message)
         if not query:
             await utils.answer(message, "Укажите запрос после команды .gpt")
             return
 
+        self.is_generating = True  # Устанавливаем флаг в True, начинается генерация ответа
         bot_id = 7072898560
-
-        # Загружаем сущность пользователя
         bot = await self.client.get_entity(bot_id)
-        
         reset_message = await self.client.send_message(bot, "/reset")
         await reset_message.delete()
-
         gpt_message = await message.edit(self.strings("generating"))
 
         async with self.client.conversation(bot) as conv:
             await conv.send_message(query)
             response = await self._get_response(conv)
-        
+
         await gpt_message.edit(
             "{}\n\n{}".format(
                 self.strings("question").format(query),
                 self.strings("answer").format(response)
             )
         )
+        self.is_generating = False  # Сбрасываем флаг после получения ответа
 
     async def _get_response(self, conv):
         start_time = time.time()
         last_edit_time = start_time
+        last_msg = None
 
         while True:
             msg = await conv.get_response()
             current_time = time.time()
 
-            if (current_time - last_edit_time) > 2:
+            if msg == last_msg and (current_time - last_edit_time) > 2:
                 return msg.text
 
-            last_edit_time = current_time
+            if msg.text != last_msg:
+                last_edit_time = current_time
+                last_msg = msg.text
 
             if msg.text == "Готово!":
                 continue
-
-            await msg.edit(msg.text)
